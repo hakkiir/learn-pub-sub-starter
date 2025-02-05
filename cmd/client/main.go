3a9127be147d81ae94f7aad41ca8530ccc7cbbcb
hakkiir <hakkaraineniiro@gmail.com>
2025-02-05 13:37:55 +0200

    basic game repl + pub/sub for pausing

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	gamelogic "github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	pubsub "github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	routing "github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const rabbitMqURL = "amqp://guest:guest@localhost:5672/"
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	fmt.Println("Starting Peril server...")
+	userName, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	rabbitConn, err := amqp.Dial(rabbitMqURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rabbitConn.Close()
+
+	fmt.Println("connection succesful")
+
+	PauseQue := routing.PauseKey + "." + userName
+	_, _, err = pubsub.DeclareAndBind(rabbitConn, routing.ExchangePerilDirect, PauseQue, routing.PauseKey, pubsub.Transient)
+	if err != nil {
+		fmt.Println(err)
+	}
+	gamestate := gamelogic.NewGameState(userName)
+	pubsub.SubscribeJSON(rabbitConn, routing.ExchangePerilDirect, PauseQue, routing.PauseKey, pubsub.Transient, handlerPause(gamestate))
+	for {
+		input := gamelogic.GetInput()
+		if len(input) == 0 {
+			continue
+		}
+		cmd := input[0]
+		switch cmd {
+		case "spawn":
+			err = gamestate.CommandSpawn(input)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		case "move":
+			_, err := gamestate.CommandMove(input)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		case "status":
+			gamestate.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			fmt.Println("Spamming not allowed yet!")
+		case "quit":
+			gamelogic.PrintQuit()
+			return
+		default:
+			fmt.Println("invalid command")
+		}
+	}
+}
+
+func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
+	return func(ps routing.PlayingState) {
+		defer fmt.Print("> ")
+		gs.HandlePause(ps)
+	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	gamelogic "github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	pubsub "github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	routing "github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const rabbitMqURL = "amqp://guest:guest@localhost:5672/"
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	gamelogic.PrintServerHelp()
+
+	rabbitConn, err := amqp.Dial(rabbitMqURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rabbitConn.Close()
+
+	fmt.Println("connection succesful")
+
+	rabbitChan, err := rabbitConn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, _, err = pubsub.DeclareAndBind(rabbitConn, routing.ExchangePerilTopic, routing.GameLogSlug, routing.GameLogSlug+".*", pubsub.Durable)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for {
+		input := gamelogic.GetInput()
+		if len(input) == 0 {
+			continue
+		}
+		if input[0] == "pause" {
+			fmt.Println("sending pause message")
+			pubsub.PublishJSON(rabbitChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: true,
+			})
+			continue
+		} else if input[0] == "resume" {
+			fmt.Println("sending resume message")
+			pubsub.PublishJSON(rabbitChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: false,
+			})
+			continue
+		} else if input[0] == "quit" {
+			fmt.Println("exiting..")
+			break
+		} else {
+			fmt.Println("not valid command")
+			continue
+		}
+	}
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, os.Interrupt)
+	<-sigchan
+	log.Println("Program shutting down, closing connection")
+
+	os.Exit(0)
+
 }
